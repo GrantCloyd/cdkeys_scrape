651b523dd2e26efb7107e7f0813628601b5c9a85
Grant Cloyd <dgcloyd@gmail.com>
2025-04-26 15:21:23 -0400

    change games, structure of data, add header and helper methods

--- cdkeys_scrape.rb
+++ cdkeys_scrape.rb
@@ -5,37 +5,60 @@ require 'open-uri'
 
 PRIMARY_URL = "https://www.cdkeys.com".freeze
 LINK_IDENTIFIERS = {
- "armored-core-vi-fires-of-rubicon-pc-steam" => 25,
-  "god-of-war-ragnarok-pc-steam-na" => 30,
-  "horizon-forbidden-west-complete-edition-pc-steam" => 25,
-  "metaphor-refantazio-pc-steam" => 25 
+  "30" => ["god-of-war-ragnarok-pc-steam-na"],
+  "25" => [
+    "horizon-forbidden-west-complete-edition-pc-steam",
+    "clair-obscur-expedition-33-pc-steam",
+    "warhammer-40-000-space-marine-2-pc-steam",
+    "the-hundred-line-last-defense-academy-pc-steam"
+  ],
+  "20" => [
+      "outriders-complete-edition-pc-steam"
+  ]
 }.freeze
 
 def execute 
-  LINK_IDENTIFIERS.each do |id, interest_price|
-    page = Nokogiri::HTML(URI.open("#{PRIMARY_URL}/#{id}"))
-  
-    name = page.css(".item.product").first&.text
-    price = page.css(".price").first&.text
-    at_or_below_interest_price = (price.gsub("$", "").to_f) <= interest_price.to_f
-    available = page.css(".product-usps-item.attribute.stock.available").first&.text
+  LINK_IDENTIFIERS.each do |interest_price, ids|
+    display_header(interest_price)
+
+    ids.each do |id|
+      page = Nokogiri::HTML(URI.open("#{PRIMARY_URL}/#{id}"))
     
-    # note, region is behind knockout js library restriction
-    # so can't be scraped from page directly
-    display_results(name, price, available, at_or_below_interest_price)
-    
-  
+      name = page.css(".item.product").first&.text
+      price = page.css(".price").first&.text
+      at_or_below_interest_price = (price.gsub("$", "").to_f) <= interest_price.to_f
+      available = page.css(".product-usps-item.attribute.stock.available").first&.text
+      
+      # note, region is behind knockout js library restriction
+      # so can't be scraped from page directly
+      display_results(name, price, available, at_or_below_interest_price)
+    end
   end
 end
 
+def display_header(interest_price)
+  newline(2)
+  line_breaker(2)
+  puts "\s\sINTEREST PRICE RANGE OF #{interest_price}"
+  line_breaker
+end
+
 def display_results(name, price, available, is_good_price)
-  puts "*" * 10
+  newline
+  line_breaker
   puts "Game: #{name} Price: #{price}"
   puts "Available: #{available}"
-  puts "\n"
+  newline
   puts "Price is #{is_good_price ? "" : "NOT"} below marked interest price."
-  puts "*" * 10
-  puts "\n"
+  newline
+end
+
+def line_breaker(times_to_perform = 1)
+  times_to_perform.times { puts "*" * 30 }
+end
+
+def newline(times_to_perform = 1)
+  times_to_perform.times { puts "\n" }
 end
 
 execute
